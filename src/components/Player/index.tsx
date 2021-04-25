b321d423e1b705a5208ae3e5218454ac9b37612a
wellingtondbjr <wellingtondbjr@gmail.com>
2021-04-25 17:09:20 -0300

    first commit

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,50 @@
+import { useContext } from 'react';
+import { PlayerContext } from '../../contexts/PlayerContext';
+import styles from './styles.module.scss';
+
+export function Player() {
+    const { episodeList, currentEpisodeIndex } = useContext(PlayerContext)
+
+    const episode = episodeList[currentEpisodeIndex]
+
+    return (
+        <div className={styles.playerContainer}>
+            <header>
+                <img src="/playing.svg" alt="Tocando agora" />
+                <strong>Tocando agora {episode?.title}</strong>
+            </header>
+
+            <div className={styles.emptyPlayer}>
+                <strong>Selecione um podcast para ouvir</strong>
+            </div>
+
+            <footer className={styles.empty}>
+                <div className={styles.progress}>
+                    <span>00:00</span>
+                    <div className={styles.slider}>
+                        <div className={styles.emptySlider} />
+                    </div>
+                    <span>00:00</span>
+                </div>
+
+                <div className={styles.buttons}>
+                    <button type="button">
+                        <img src="/shuffle.svg" alt="Embaralhar" />
+                    </button>
+                    <button type="button">
+                        <img src="/play-previous.svg" alt="Tocar anterior" />
+                    </button>
+                    <button type="button" className={styles.playButton}>
+                        <img src="/play.svg" alt="Tocar" />
+                    </button>
+                    <button type="button">
+                        <img src="/play-next.svg" alt="Tocar próxima" />
+                    </button>
+                    <button type="button">
+                        <img src="/repeat.svg" alt="Repetir" />
+                    </button>
+                </div>
+            </footer>
+        </div>
+    );
+}
